ed0ff9c19e4f837a5700667db477195c1be528b0
coldarmy <coldarmy@gmail.com>
2022-05-21 16:52:41 -0400

    explosions add force

--- Top Down Shooter/Assets/_Scripts/ExplosionController.cs
+++ Top Down Shooter/Assets/_Scripts/ExplosionController.cs
@@ -1,12 +1,13 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ExplosionController : MonoBehaviour
 {
-    [SerializeField] private float lifeTime, growSpeed;
+    [SerializeField] private float lifeTime, explosioinForce;
+    [SerializeField] private Vector3 finalScale;
     public int damage;
-    private float curLife;
     private Poolable _poolable;
 
     private void OnEnable()
@@ -22,21 +23,35 @@ public class ExplosionController : MonoBehaviour
     private void Update()
     {
         
-        transform.localScale += (Vector3.one * growSpeed * Time.deltaTime);
+      /*  transform.localScale += (Vector3.one * growSpeed * Time.deltaTime);
         curLife += Time.deltaTime;
         if(curLife >= lifeTime)
         {
             Disable();
-        }
+        }*/
 
     }
 
     public void SpawnExplosion(int dmg)
     {
         damage = dmg;
-        curLife = 0;
+      //  curLife = 0;
         transform.localScale = Vector3.zero;
         gameObject.SetActive(true);
+        float radius = finalScale.x;
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+            if (rb != null)
+                rb.AddExplosionForce(explosioinForce, transform.position, radius);
+        }
+        transform.DOScale(finalScale, lifeTime).OnComplete(() =>
+        {
+           gameObject.SetActive(false);
+        });
+
     }
 
     private void Disable()
